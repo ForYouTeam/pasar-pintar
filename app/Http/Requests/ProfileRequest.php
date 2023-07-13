3a6da3a72be1e48160dbd874961858a1c288ab57
nzrkodi <nzrkodi@gmail.com>
2023-07-13 13:33:37 +0800

    membuat tampilan pasar pintar

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nama_pemilik' => 'required | min:2 | max:150',
+            'nama_usaha'   => 'required | min:2 | max:150',
+            'alamat'       => 'required | min:2 | max:150',
+            'telepon'      => 'required | min:2 | max:150',
+        ];
+    }
+
+       protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
+    }
+}
